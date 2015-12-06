298a33afae523d088553924013cf0247dacdb64a
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2015-12-06 01:48:36 +0000

    Insercao de dados na DataWarehouse

--- Data Warehouse/dataWarehouse - create Table.sql
+++ Data Warehouse/dataWarehouse - create Table.sql
@@ -3,9 +3,9 @@ CREATE TABLE dataWarehouse(
 	dia INT NOT NULL,
     mes INT NOT NULL,
     ano INT NOT NULL,
-    media float NOT NULL,
+    numero_tentativas_login INT NOT NULL,
     FOREIGN KEY (email) REFERENCES d_utilizador (email) ON DELETE CASCADE,
-    FOREIGN KEY (dia, mes, ano) REFERENCES d_tempo (dia, mes, ano) ON DELETE CASCADE 
+    FOREIGN KEY (dia, mes, ano) REFERENCES d_tempo (dia, mes, ano)
 );
 
 CREATE TABLE d_utilizador(
@@ -22,3 +22,11 @@ CREATE TABLE d_tempo(
     ano INT NOT NULL,
 PRIMARY KEY (dia, mes, ano)
 );
+
+
+INSERT d_utilizador SELECT email, nome, pais, categoria FROM utilizador;
+INSERT d_tempo (SELECT DATE_FORMAT(moment, '%d'), DATE_FORMAT(moment, '%m'), DATE_FORMAT(moment, '%Y') from sequencia);
+INSERT dataWarehouse 	(SELECT B.email, DATE_FORMAT(A.moment, '%d'), DATE_FORMAT(A.moment, '%m'), DATE_FORMAT(A.moment, '%Y'), COUNT(*)
+						FROM login A, utilizador B
+						WHERE A.userid=B.userid
+						GROUP BY A.moment, B.email);
