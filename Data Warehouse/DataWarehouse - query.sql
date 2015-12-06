722456e0c5513d2a852ec9421541f0769648400f
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2015-12-06 16:25:23 +0000

    Update bug Data Warehouse

--- Data Warehouse/DataWarehouse - query.sql
+++ Data Warehouse/DataWarehouse - query.sql
@@ -1,5 +1,5 @@
 SELECT U.categoria, U.email, D.mes, D.ano, D.Media_Tentativas_Login
-FROM d_utilizador U,   (SELECT A.email, A.mes, A.ano, B.pais, AVG(A.numero_tentativas_login) AS Media_Tentativas_Login
+FROM d_utilizador U,   (SELECT A.email, A.mes, A.ano, AVG(A.numero_tentativas_login) AS Media_Tentativas_Login
 						FROM dataWarehouse A, d_utilizador B
 						WHERE A.email=B.email 
 						GROUP BY A.email, A.ano, A.mes WITH ROLLUP) D
