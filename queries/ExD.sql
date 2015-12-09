9f79eeff0cadedfbe55f99669e7f5321ba74c4b4
luisrafael1995 <luis_rafael_1995@hotmail.com>
2015-12-09 23:18:55 +0000

    querie D à moda do stor

--- queries/ExD.sql
+++ queries/ExD.sql
@@ -1,17 +1,11 @@
-DROP VIEW IF EXISTS todosAtivos;
-CREATE VIEW todosAtivos AS (
-	SELECT R.userid, T.typecnt typeid,  R.regcounter regid, P.pagecounter pageid, R.nome
-    FROM tipo_registo T, registo R, pagina P, reg_pag RP
-    WHERE T.userid=R.userid AND T.userid=RP.userid AND T.userid=P.userid
-        AND T.typecnt=R.typecounter AND T.typecnt=RP.typeid
-        AND R.regcounter=RP.regid AND P.pagecounter=RP.pageid
-        AND T.ativo AND R.ativo AND P.ativa AND RP.ativa);
-
-SELECT DISTINCT userid
-FROM tipo_registo
-WHERE ativo AND (userid) NOT IN (
-	SELECT DISTINCT userid
-	FROM tipo_registo
-	WHERE ativo AND (userid, typecnt) NOT IN (
-		SELECT DISTINCT userid, typeid typecnt
-		FROM todosAtivos));
+SELECT U.userid
+FROM utilizador U
+WHERE NOT EXISTS (
+    SELECT *
+    FROM tipo_registo T
+    WHERE U.userid = T.userid
+        AND NOT EXISTS (
+        SELECT *
+        FROM reg_pag R
+        WHERE T.userid = R.userid
+            AND T.typecnt = R.typeid));
