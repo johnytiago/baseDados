8babea7fc68120e3d3ad20f73bc47591ab3d05ab
luisrafael1995 <luis_rafael_1995@hotmail.com>
2015-12-09 13:03:35 +0000

    ExB à moda do stor

--- queries/ExB.sql
+++ queries/ExB.sql
@@ -1,23 +1,11 @@
-DROP VIEW IF EXISTS todosAtivos;
-CREATE VIEW todosAtivos AS (
-	SELECT R.userid, T.typecnt typeid,  R.regcounter regid, P.pagecounter pageid, R.nome
-    FROM tipo_registo T, registo R, pagina P, reg_pag RP
-    WHERE T.userid=R.userid AND T.userid=RP.userid AND T.userid=P.userid
-        AND T.typecnt=R.typecounter AND T.typecnt=RP.typeid
-        AND R.regcounter=RP.regid AND P.pagecounter=RP.pageid
-        AND T.ativo AND R.ativo AND P.ativa AND RP.ativa);
-
-SELECT DISTINCT userid, typeid, regid, nome
-FROM todosAtivos A
-WHERE (userid, typeid, regid) NOT IN (
-    SELECT B1.userid, typeid, regid
-    FROM (SELECT userid, typeid,  regid
-        FROM todosAtivos) B1,
-        (SELECT userid, pagecounter pageid
-        FROM pagina
-        WHERE ativa) B2
-    WHERE B1.userid = B2.userid
-        AND (B1.userid, typeid, regid, pageid) NOT IN (
-            SELECT userid, typeid,  regid, pageid
-            FROM todosAtivos))
-ORDER BY userid;
+SELECT R.userid, R.typecounter, R.regcounter, R.nome
+FROM registo R
+WHERE NOT EXISTS (
+    SELECT *
+    FROM pagina P
+    WHERE P.userid = R.userid
+        AND NOT EXISTS (
+            SELECT *
+            FROM reg_pag RP
+            WHERE R.userid = RP.userid AND R.typecounter = RP.typeid
+                AND R.regcounter = RP.regid AND P.pagecounter = RP.pageid));
