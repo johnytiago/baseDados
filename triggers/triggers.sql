353b918fd863c6d93585a7b21591d4e801455ad7
Pedro Oliveira <pedrofran12@users.noreply.github.com>
2015-12-09 12:19:20 +0000

    Identações e reg_pag

--- triggers/triggers.sql
+++ triggers/triggers.sql
@@ -1,77 +1,76 @@
-DROP FUNCTION IF EXISTS check_seq_func;
 DELIMITER $$
+DROP FUNCTION IF EXISTS check_seq_func $$
 CREATE FUNCTION check_seq_func(seq_id INT) RETURNS INT
 BEGIN
   RETURN EXISTS ( SELECT *
-     FROM (select idseq from tipo_registo
-       UNION
-       select idseq from pagina
-       UNION
-       select idseq from campo
-       UNION
-       select idseq from registo
-       UNION
-       select idseq from valor) A
+     FROM (SELECT idseq FROM tipo_registo
+		   UNION
+		   SELECT idseq FROM pagina
+		   UNION
+		   SELECT idseq FROM campo
+		   UNION
+		   SELECT idseq FROM registo
+		   UNION
+		   SELECT idseq FROM valor) A
      WHERE A.idseq=seq_id);
 END$$
-DELIMITER ;
 
-DROP TRIGGER IF EXISTS check_seq_tipo_registo;
-DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_seq_tipo_registo $$
 CREATE TRIGGER check_seq_tipo_registo
     BEFORE INSERT ON tipo_registo FOR EACH ROW
-    BEGIN
-        IF (SELECT check_seq_func(NEW.idseq))
-  THEN
-   set NEW=null;
-  END IF;
-    END$$
-DELIMITER ;
+BEGIN
+	IF (SELECT check_seq_func(NEW.idseq))
+		THEN set NEW=null;
+	END IF;
+END$$
 
-DROP TRIGGER IF EXISTS check_seq_pagina;
-DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_seq_pagina $$
 CREATE TRIGGER check_seq_pagina
     BEFORE INSERT ON pagina FOR EACH ROW
-    BEGIN
-        IF (SELECT check_seq_func(NEW.idseq))
-  THEN
-   set NEW=null;
-  END IF;
-    END$$
-DELIMITER ;
+BEGIN
+	IF (SELECT check_seq_func(NEW.idseq))
+		THEN set NEW=null;
+	END IF;
+END$$
 
-DROP TRIGGER IF EXISTS check_seq_campo;
-DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_seq_campo $$
 CREATE TRIGGER check_seq_campo
     BEFORE INSERT ON campo FOR EACH ROW
-    BEGIN
-        IF (SELECT check_seq_func(NEW.idseq))
-  THEN
-   set NEW=null;
-  END IF;
-    END$$
-DELIMITER ;
+BEGIN
+	IF (SELECT check_seq_func(NEW.idseq))
+		THEN set NEW=null;
+	END IF;
+END$$
 
-DROP TRIGGER IF EXISTS check_seq_registo;
-DELIMITER $$
+DROP TRIGGER IF EXISTS check_seq_registo $$
 CREATE TRIGGER check_seq_registo
     BEFORE INSERT ON registo FOR EACH ROW
-    BEGIN
-        IF (SELECT check_seq_func(NEW.idseq))
-  THEN
-   set NEW=null;
-  END IF;
-    END$$
-DELIMITER ;
+BEGIN
+	IF (SELECT check_seq_func(NEW.idseq))
+		THEN set NEW=null;
+	END IF;
+END$$
 
-DROP TRIGGER IF EXISTS check_seq_valor;
-DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_seq_valor $$
 CREATE TRIGGER check_seq_valor
     BEFORE INSERT ON valor FOR EACH ROW
-    BEGIN
-        IF (SELECT check_seq_func(NEW.idseq))
-  THEN
-   set NEW=null;
-  END IF;
-    END$$
+BEGIN
+	IF (SELECT check_seq_func(NEW.idseq))
+		THEN set NEW=null;
+	END IF;
+END$$
+
+
+DROP TRIGGER IF EXISTS check_seq_reg_pag $$
+CREATE TRIGGER check_seq_reg_pag
+    BEFORE INSERT ON reg_pag FOR EACH ROW
+BEGIN
+	IF (SELECT check_seq_func(NEW.idseq))
+		THEN set NEW=null;
+	END IF;
+END$$
 DELIMITER ;
