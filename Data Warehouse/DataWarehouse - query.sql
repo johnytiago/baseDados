402c730f00493763a64e1727f20814a9bdbdd52b
Pedro Oliveira <pedrofran12@users.noreply.github.com>
2015-12-09 12:33:43 +0000

    Update nome da tabela

--- Data Warehouse/DataWarehouse - query.sql
+++ Data Warehouse/DataWarehouse - query.sql
@@ -1,6 +1,6 @@
 SELECT U.categoria, U.email, D.mes, D.ano, D.Media_Tentativas_Login
 FROM d_utilizador U,   (SELECT A.email, A.mes, A.ano, AVG(A.numero_tentativas_login) AS Media_Tentativas_Login
-						FROM dataWarehouse A, d_utilizador B
+						FROM dataWarehouse_login A, d_utilizador B
 						WHERE A.email=B.email 
 						GROUP BY A.email, A.ano, A.mes WITH ROLLUP) D
 WHERE U.email=D.email AND U.pais='Portugal'
