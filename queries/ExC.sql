894787ec04b4c5f296736c8cc8fc283a35394223
Pedro Oliveira <pedrofran12@users.noreply.github.com>
2015-12-09 13:14:13 +0000

    Remocao de ativos

--- queries/ExC.sql
+++ queries/ExC.sql
@@ -1,14 +1,6 @@
-DROP VIEW IF EXISTS todosAtivos;
-CREATE VIEW todosAtivos AS (
-	SELECT R.userid, T.typecnt typeid,  R.regcounter regid, P.pagecounter pageid, R.nome
-    FROM tipo_registo T, registo R, pagina P, reg_pag RP
-    WHERE T.userid=R.userid AND T.userid=RP.userid AND T.userid=P.userid
-        AND T.typecnt=R.typecounter AND T.typecnt=RP.typeid
-        AND R.regcounter=RP.regid AND P.pagecounter=RP.pageid
-        AND T.ativo AND R.ativo AND P.ativa AND RP.ativa);
-
-SELECT A.userid, B.nome, B.email, COUNT(distinct pageid) AS NumPags, count(*) NumRegs, count(*)/COUNT(distinct pageid) AS NumeroMedioRegistosPorPagina
-FROM  todosAtivos A, utilizador B
-WHERE A.userid=B.userid
+SELECT U.userid, U.nome, U.email, COUNT(RP.regid) AS NumRegs, COUNT(DISTINCT P.pagecounter) AS NumPags, count(RP.regid)/COUNT(DISTINCT P.pagecounter) AS NumeroMedioRegistosPorPagina
+FROM utilizador U, pagina P LEFT JOIN reg_pag RP
+	ON P.pagecounter=RP.pageid AND P.userid=RP.userid
+WHERE U.userid=P.userid
 GROUP BY userid, nome, email
 ORDER BY 6 DESC;
