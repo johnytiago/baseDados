78a911f1b5256d1050243ab05db6a53d8a1554ad
Pedro Oliveira <pedrofran12@users.noreply.github.com>
2015-12-10 15:15:35 +0000

    Update indices - sem ativos

--- index/QueriesIndex.sql
+++ index/QueriesIndex.sql
@@ -1,21 +1,13 @@
-DROP VIEW IF EXISTS todosAtivos;
-CREATE VIEW todosAtivos AS (
-	SELECT SQL_NO_CACHE R.userid, T.typecnt typeid,  R.regcounter regid, P.pagecounter pageid, R.nome
-    FROM tipo_registo T, registo R, pagina P, reg_pag RP
-    WHERE T.ativo AND R.ativo AND P.ativa AND RP.ativa
-		AND T.userid=R.userid AND T.userid=RP.userid AND T.userid=P.userid
-        AND T.typecnt=R.typecounter AND T.typecnt=RP.typeid
-        AND R.regcounter=RP.regid AND P.pagecounter=RP.pageid);
-
-
 -- alinea a
-SELECT SQL_NO_CACHE A.userid, B.nome, B.email, COUNT(distinct pageid) AS NumPags, count(*) NumRegs, count(*)/COUNT(distinct pageid) AS NumeroMedioRegistosPorPagina
-FROM  todosAtivos A, utilizador B
-WHERE A.userid=B.userid
+SELECT SQL_NO_CACHE U.userid, U.nome, U.email, COUNT(RP.regid) AS NumRegs, COUNT(DISTINCT P.pagecounter) AS NumPags, count(RP.regid)/COUNT(DISTINCT P.pagecounter) AS NumeroMedioRegistosPorPagina
+FROM utilizador U, pagina P LEFT JOIN reg_pag RP
+	ON P.pagecounter=RP.pageid AND P.userid=RP.userid
+WHERE U.userid=P.userid
 GROUP BY userid, nome, email
 ORDER BY 6 DESC;
 
 -- alinea b
-SELECT SQL_NO_CACHE userid, pageid, nome AS Nome_Registo
-FROM todosAtivos
-ORDER BY userid, pageid, Nome_Registo
+SELECT SQL_NO_CACHE RP.userid, RP.pageid, RP.regid, R.nome AS Nome_Registo
+FROM reg_pag RP, registo R
+WHERE RP.userid=R.userid AND RP.regid=R.regcounter
+ORDER BY RP.userid, RP.pageid, RP.regid;
